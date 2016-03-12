1738d6a860a214261c447aa819668bd372ccb2b7
rbcasperson <casperson.ryan@gmail.com>
2016-03-12 14:21:54 -0500

    manually assign board.orbs in test
    For some reason, this test is failing even though the expected and actual both read 2,4,down,3.

--- test/board.spec.js
+++ test/board.spec.js
@@ -20,7 +20,17 @@ test('creates seven different types of orbs', t => {
 });
 
 test('finds all matches', t => {
-    t.same(board.evaluate(), []);
+    board.orbs = [ 
+        [ 1, 6, 4, 6, 1, 5, 3, 2 ],
+        [ 3, 6, 5, 4, 5, 0, 6, 5 ],
+        [ 1, 2, 6, 6, 1, 4, 0, 0 ],
+        [ 5, 4, 0, 1, 1, 0, 4, 4 ],
+        [ 4, 4, 2, 2, 1, 4, 5, 6 ],
+        [ 5, 1, 2, 6, 0, 2, 2, 3 ],
+        [ 5, 2, 6, 0, 3, 5, 5, 2 ],
+        [ 6, 1, 5, 5, 6, 4, 3, 6 ] 
+    ];
+    t.same(board.evaluate(), [[2, 4], 'down', 3]);
 });
 
 test.skip('should have at least one possible match set by default', t => {
