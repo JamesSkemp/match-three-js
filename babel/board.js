05adc89515e3ee48baf3a8c45f6dd66222e0881a
Droogans <droogans@gmail.com>
2016-03-18 16:53:42 -0500

    Separate out orb-manipulating functions from board class instances

--- babel/board.js
+++ babel/board.js
@@ -12,7 +12,7 @@ let SortedSet = require('collections/sorted-set');
  * [ [ [6, 5, 4], [3, 2, 2] ], [ [3, 2, 2], [6, 4, 0] ],
  *   [ [6, 3, 6], [5, 2, 4] ], [ [5, 2, 4], [4, 2, 0] ] ]
  */
-let iterchunks = function (orbs, chunkLimitRange = [4, 2]) {
+export function iterchunks (orbs, chunkLimitRange = [4, 2]) {
     // Defaults will find all possible 3-matches. Used by `Board#evaluate`.
     let [width, height] = chunkLimitRange;
     let [finalPositionWidth, finalPositionHeight] = [orbs[0].length - width, orbs.length - height];
@@ -28,7 +28,49 @@ let iterchunks = function (orbs, chunkLimitRange = [4, 2]) {
     return chunks;
 };
 
-export default class Board {
+export function findMatches(orbs) {
+    let matches = [];
+    let b = orbs;
+    let height = orbs.length;
+    let width = orbs[0].length;
+
+    _.each(_.range(height - 2), x => {
+        _.each(_.range(width - 2), y => {
+            if (b[x][y] == b[x][y + 1] && b[x][y] == b[x][y + 2]) {
+                matches.push([[x, y], [x, y + 1], [x, y + 2]])
+            } else if (b[x][y] == b[x + 1][y] && b[x][y] == b[x + 2][y]) {
+                matches.push([[x, y], [x + 1, y], [x + 2, y]])
+            };
+        });
+    });
+    return matches;
+}
+
+export function combineMatches(matches) {
+    let combinedMatches = [];
+    let unused = matches;
+    let couldMatch, before, currentMatch;
+
+    while (unused[0] != null) {
+        currentMatch = new SortedSet(unused[0]);
+        unused.shift();
+        couldMatch = _.clone(unused);
+
+        _.each(couldMatch, m => { //only union if there is an overlap!
+            if (currentMatch.intersection(m).toArray()[0] != null) {
+                before = currentMatch.toArray();
+                currentMatch.swap(0, currentMatch.length, currentMatch.union(m));
+                if (before != currentMatch.toArray()) {
+                    unused.splice(unused.indexOf(m), 1);
+                }
+            }
+        });
+        combinedMatches.push(currentMatch.toArray());
+    }
+    return combinedMatches;
+}
+
+export class Board {
     constructor (width = 8, height = 8, types = _.range(7)) {
         this.width = width;
         this.height = height;
@@ -47,48 +89,8 @@ export default class Board {
         return _.uniq(_.flatten(this.orbs)).sort();
     }
 
-    findMatches() {
-        let matches = [],
-            b = _.cloneDeep(this.orbs);
-        
-        _.each(_.range(this.height - 2), x => {
-            _.each(_.range(this.width - 2), y => {
-                if (b[x][y] == b[x][y + 1] && b[x][y] == b[x][y + 2]) {
-                    matches.push([[x, y], [x, y + 1], [x, y + 2]])
-                } else if (b[x][y] == b[x + 1][y] && b[x][y] == b[x + 2][y]) {
-                    matches.push([[x, y], [x + 1, y], [x + 2, y]])
-                };
-            });
-        });
-        return matches;
-    }
-    
-    combineMatches() {
-        let combinedMatches = [],
-            unused = _.clone(this.findMatches()), 
-            couldMatch, before, currentMatch;
-        
-        while (unused[0] != null) {
-            currentMatch = new SortedSet(unused[0]);
-            unused.shift();
-            couldMatch = _.clone(unused);
-                
-            _.each(couldMatch, m => { //only union if there is an overlap!
-                if (currentMatch.intersection(m).toArray()[0] != null) {
-                    before = currentMatch.toArray();
-                    currentMatch.swap(0, currentMatch.length, currentMatch.union(m));
-                    if (before != currentMatch.toArray()) {
-                        unused.splice(unused.indexOf(m), 1);
-                    }
-                }
-            });
-            combinedMatches.push(currentMatch.toArray());
-        }
-        return combinedMatches;
-    }
-    
     evaluate() {
-        
+
     }
 
     evaluateAll() {
