8fef259a72ed37351bc7df69b7e37c15a96d3a9f
rbcasperson <casperson.ryan@gmail.com>
2016-05-06 20:24:50 -0400

    Improve readability for unmatch

--- babel/src/board.js
+++ babel/src/board.js
@@ -297,12 +297,18 @@ export class Board {
         };
     }
     
-    
+    /**
+      * @private
+      * @description Does the dirty work for unmatch by taking a given orb and swapping with a neighbor
+      * of a different type, or a random orb on the board if there are no valid neighbors.
+      * @see unmatch
+      */
     _unmatch(row, col, match, skipToRandom = false) {
         let thisOrb = this.orbs[row][col];
         let swapped = false;
         let directions = _.shuffle(['up', 'down', 'left', 'right']);
         for (let i = 0; i < 4; i++) {
+            // abandons the process and jumps to swapping a random orb
             if (skipToRandom) { break };
             if (directions[i] === 'up' && !_.isUndefined(this.orbs[row - 1]) && this.orbs[row - 1][col] !== thisOrb) {
                 this.swap([[row, col], [row - 1, col]], false);
@@ -331,22 +337,56 @@ export class Board {
         }
     }
     
+    /**
+      * @description Removes all match events one at a time by swapping a median or intersecting orb
+      * with its neighbor or a random orb if necessary. 
+      *
+      * Intersectiions only occur if the match is not a simple match, i.e. it only spans one
+      * column or one row.
+      *
+      * @example A simple match could go from 
+      *         [ 1, 2, 3, 4 ],             [ 1, 2, 5, 4 ],
+      *         [ 2, 5, 5, 5 ],             [ 2, 5, 3, 5 ],
+      *         [ 3, 4, 1, 2 ],     to      [ 3, 4, 1, 2 ],
+      *         [ 4, 1, 2, 3 ]              [ 4, 1, 2, 3 ]
+      *
+      * @example A multidimensional match could go from 
+      *         [ 1, 2, 3, 4 ],             [ 1, 5, 3, 4 ],
+      *         [ 2, 5, 5, 5 ],             [ 2, 2, 5, 5 ],
+      *         [ 3, 5, 1, 2 ],     to      [ 3, 5, 1, 2 ],     this.orbs[1][1] was the
+      *         [ 4, 5, 2, 3 ]              [ 4, 5, 2, 3 ]      intersection swapped
+      *
+      * @example A side-by-side match could go from 
+      *         [ 1, 5, 5, 5 ],             [ 1, 5, 4, 5 ],                 [ 1, 5, 4, 5 ],
+      *         [ 2, 5, 5, 5 ],             [ 2, 5, 5, 5 ],                 [ 2, 5, 1, 5 ],
+      *         [ 3, 4, 1, 2 ],     to      [ 3, 4, 1, 2 ],    and then     [ 3, 4, 5, 2 ],
+      *         [ 4, 1, 2, 3 ]              [ 5, 1, 2, 3 ]                  [ 5, 1, 2, 3 ]
+      */
     unmatch() {
         while (this.hasMatchEvent()) {
             let intersections = [];
             let match = combineMatches(findMatches(this.orbs))[0];
+            // it is a simple match if all of the coords have only 1 rowCoord or 1 colCoord
             let [rowCoords, colCoords] = _.zip(...match);
-            if (_.uniq(rowCoords).length === 1 || _.uniq(colCoords).length === 1) {
+            let isSimpleMatch = _.uniq(rowCoords).length === 1 || _.uniq(colCoords).length === 1;
+            if (isSimpleMatch) {
+                // finds the median orb in the match
+                let median = Math.floor(match.length / 2);
+                let [midRow, midCol] = match[median];
+
+                // Checks for a side-by-side match, which could cause and endless loop.
+                // In that case, the skipToRandom argument in _unmatch is triggered.
                 let midNeighbors;
-                let [midRow, midCol] = match[Math.floor(match.length / 2)];
                 if (_.uniq(rowCoords).length === 1) {
                     midNeighbors = [this.orbs[midRow][midCol - 1], this.orbs[midRow][midCol + 1]];
                 } else {
                     midNeighbors = [this.orbs[midRow - 1][midCol], this.orbs[midRow + 1][midCol]];
                 }
                 let isSideBySideMatch = _.includes(midNeighbors, this.orbs[midRow][midCol]);
-                this._unmatch(...match[Math.floor(match.length / 2)], match, isSideBySideMatch);
+
+                this._unmatch(...match[median], match, isSideBySideMatch);
             } else {
+                // collects which rows and columns have matches in them
                 let matchRows = [];
                 let matchCols = [];
                 _.each(_.countBy(rowCoords), (v, k) => {
@@ -359,11 +399,13 @@ export class Board {
                         matchCols.push(_.toInteger(k));
                     };
                 });
+                // if a coordinate is in a row match and a column match, it is an intersection
                 _.each(match, coords => {
                     if (_.includes(matchRows, coords[0]) && _.includes(matchCols, coords[1])) {
                         intersections.push(coords);
                     };
                 });
+                // chooses a random intersection to unmatch 
                 this._unmatch(..._.sample(intersections), match);
             };
         };
