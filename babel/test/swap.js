fec90a98115f619d282642d6cb95b92a3b12d3bc
rbcasperson <casperson.ryan@gmail.com>
2016-05-10 16:58:26 -0400

    Finish test updates for swap and unmatch and remove unneeded test data files
    Fixes #20

--- babel/test/swap.js
+++ babel/test/swap.js
@@ -1,25 +1,18 @@
 import test from 'ava';
 import * as _ from 'lodash';
-import {names} from './data/names';
-import {orbs} from './data/orbs';
-import {orbsBeforeSwap} from './data/swap';
-import {swapOrbs} from './data/swap';
 import {Board} from '../src/board';
-let board;
-let swappedOrbs;
+import {boards} from './data/boards';
+let boardInstance = new Board(5, 5);
+let swappedOrbs = {};
 
-test.before(() => {
-    board = new Board(5, 5);
-    swappedOrbs = [];
-    _.each(_.range(orbs.length), i => {
-        board.orbs = _.cloneDeep(orbsBeforeSwap[i]);
-        board.swap(swapOrbs[i]);
-        swappedOrbs.push(board.orbs);
-    });
+_.each(boards, (metadata, name) => {
+    boardInstance.orbs = _.cloneDeep(metadata.swap.orbsBeforeSwap);
+    boardInstance.swap(metadata.swap.swapOrbs);
+    swappedOrbs[name] = boardInstance.orbs;
 });
 
-_.each(_.range(orbs.length), i => {
-    test(`swaps into ${names[i]}`, t => {
-       t.ok(_.isEqual(swappedOrbs[i], orbs[i]));
+_.each(boards, (metadata, name) => {
+    test(`swaps into ${name}`, t => {
+       t.ok(_.isEqual(swappedOrbs[name], metadata.orbs));
     });
 });
