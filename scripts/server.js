afb88eed71395317a63b45b94858832fb52319dd
rbcasperson <casperson.ryan@gmail.com>
2016-06-14 09:42:09 -0400

    Link README.md into gh-pages index.html

--- scripts/server.js
+++ scripts/server.js
@@ -5,7 +5,7 @@ const execSync = require('child_process').execSync;
 
 let browserSync = require('browser-sync').create();
 
-execSync('npm run babel; npm run browserify', { stdio: [0, 1, 2] });
+execSync('npm run babel; npm run browserify; node site/scripts/generate_readme.js', { stdio: [0, 1, 2] });
 
 browserSync.init({
     server: 'site',
@@ -30,5 +30,11 @@ browserSync.watch('babel/src/*.js', (event) => {
     if (event === 'change') {
         execSync('npm run babel', { stdio: [0, 1, 2] });
         execSync('npm run browserify', { stdio: [0, 1, 2] });
+    }
+});
+
+browserSync.watch('site/scripts/generate_readme.js', (event) => {
+    if (event === 'change') {
+        execSync('node site/scripts/generate_readme.js', { stdio: [0, 1, 2] });
     }
 });
