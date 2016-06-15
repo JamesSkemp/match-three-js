3c0306a3cec02dacab1acba24e769ca14bf89480
rbcasperson <casperson.ryan@gmail.com>
2016-06-15 12:41:48 -0400

    Make the site & demo look a little nicer

--- site/scripts/generate_readme.js
+++ site/scripts/generate_readme.js
@@ -5,4 +5,7 @@ var md = new Remarkable();
 var readMe = fs.readFileSync('README.md', 'utf-8');
 var markdownReadMe = md.render(readMe);
 
+// add link to stylesheet
+markdownReadMe += '<link rel="stylesheet" type="text/css" href="stylesheet.css">';
+
 fs.writeFileSync('./site/README.html', markdownReadMe);
